2602a30c35d3334a4b3a1ec934aa12cf54b26c92
Mario Flores Jr <m.flores4821@gmail.com>
2018-05-07 17:50:06 -0700

    scaffolding

---
+++ src/__test__/auth-router.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+import superagent from 'superagent';
+import { startServer, stopServer } from '../lib/server';
+import { pRemoveAccountMock } from './lib/account-mock';
+
+const apiURL = `http://localhost:${process.env.PORT}/signup`;
+
+describe('AUTH Router', () => {
+  beforeAll(startServer);
+  afterAll(stopServer);
+  afterEach(pRemoveAccountMock);
+
+  test('POST should return a 200 status code and a TOKEN', () => {
+    return superagent.post(apiURL)
+      .send({
+        username: 'placeholder',
+        email: 'placeholder@placeholder.com',
+        password: 'placeholder',
+      })
+      .then((response) => {
+        expect(response.status).toEqual(200);
+        expect(response.body.token).toBeTruthy();
+      });
+  });
+});
---
+++ src/model/account.js
@@ -0,0 +1,73 @@
+'use strict';
+
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import crypto from 'crypto';
+import jsonWebToken from 'jsonwebtoken';
+import HttpError from 'http-errors';
+
+const HASH_ROUNDS = 8;
+const TOKEN_SEED_LENGTH = 128;
+
+const accountSchema = mongoose.Schema({
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  tokenSeed: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdOn: {
+    type: Date,
+    default: () => new Date(),
+  },
+});
+
+function verifyPassword(password) {
+  return bcrypt.compare(password, this.passwordHash)
+    .then((result) => {
+      if (!result) {
+        throw new HttpError(400, 'AUTH - incorrect data');
+      }
+      return this;
+    });
+}
+
+function pCreateToken() {
+  this.tokenSeed = crypto.randomBytes(TOKEN_SEED_LENGTH).toString('hex');
+  return this.save()
+    .then((account) => {
+      return jsonWebToken.sign(
+        { tokenSeed: account.tokenSeed },
+        process.env.SOUND_CLOUD_SECRET,
+      );
+    });
+// TODO: error management
+}
+
+accountSchema.methods.pCreateToken = pCreateToken;
+
+const Account = mongoose.model('account', accountSchema);
+
+Account.create = (username, email, password) => {
+  return bcrypt.hash(password, HASH_ROUNDS)
+    .then((passwordHash) => {
+    password = null; //eslint-disable-line
+      const tokenSeed = crypto.randomBytes(TOKEN_SEED_LENGTH).toString('hex');
+      return new Account({
+        username,
+        email,
+        passwordHash,
+        tokenSeed,
+      }).save();
+    });
+};
+
+export default Account;
